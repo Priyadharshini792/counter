7196e04169ee28527d90c049c2645d52a1dd0cb9
priyadharshini <priyadharshini20160@gmail.com>
2026-03-25 21:02:49 +0530

    version1

---
+++ counter.js
@@ -0,0 +1,18 @@
+const Counter =document.getElementById("Counter");
+const decrement =document.getElementById("decrement");
+const increment =document.getElementById("increment");
+const reset =document.getElementById("reset");
+let count=0;
+
+increment.onclick=function(){
+    count++;
+    Counter.textContent=count;
+}
+reset.onclick=function(){
+    count=0;
+    Counter.textContent=count;
+}
+decrement.onclick =function(){
+    count--;
+    Counter.textContent=count;
+}
